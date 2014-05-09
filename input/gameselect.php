6f82e48d3b59244c7619dfcc2150951c886d06fb
TH.Laptop <THansenite@gmail.com>
2014-05-09 17:06:06 -0500

    added input file for data entry files; moved proto_gameInput there and added game select framework to take you to game input eventually

---
+++ input/gameselect.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+<style>
+table {
+	width: 700px;
+	border: 2px solid black;
+	border-collapse: collapse;
+}
+
+td, th {
+	border: 1px solid black;
+	padding: 4px;
+}
+
+th {
+	border-bottom: 2px solid black;
+}
+
+</style>
+
+</head>
+<body>
+
+<h2>Edit Games</h2>
+
+<table id="select_game">
+<thead>
+<tr>
+	<th>Date</th>
+	<th>Time</th>
+	<th>Home</th>
+	<th>Away</th>
+	<th>Edit</th>
+</tr>
+</thead>
+<tbody>
+<?php
+include ("../../setup/db_setup.php");
+
+$connection = mysqli_connect($server, $username, $password, $database) or die ("Connection failed");
+
+$query = "select date_format(sched.date,'%c/%e/%y') as 'date', time_format(sched.time,'%h:%i %p') as 'time', team1.name as 'home', team2.name as 'away', sched.id as 'id' from schedule sched join team team1 on sched.home = team1.id join team team2 on sched.away = team2.id order by sched.date, time";
+$result = mysqli_query($connection, $query) or die ("Query failed");
+
+while ($row = mysqli_fetch_assoc($result)) {
+	$date = $row['date'];
+	$time = $row['time'];
+	$home = $row['home'];
+	$away = $row['away'];
+	$id = $row['id'];
+
+	echo "<tr><td>";
+	echo htmlentities($date);
+	echo "</td><td>";
+	echo htmlentities($time);
+	echo "</td><td>";
+	echo htmlentities($home);
+	echo "</td><td>";
+	echo htmlentities($away);
+	echo "</td><td>";
+	echo "<a href = \"#\">Edit game ";
+	echo htmlentities($id);
+	echo "</a></td></tr>";
+}
+mysqli_free_result($result);
+mysqli_close($connection);
+?>
+
+</tbody>
+</table>
+
+
+</body>
+</html>
---
+++ input/proto_gameInput.php
@@ -0,0 +1,165 @@
+<html>
+<head>
+<style>
+table {
+	width: 700px;
+	border: 2px solid black;
+	border-collapse: collapse;
+}
+
+td, th {
+	border: 1px solid black;
+	padding: 4px;
+}
+
+th {
+	border-bottom: 2px solid black;
+}
+
+</style>
+
+</head>
+<body>
+
+<table id="schedule_table">
+<tr>
+	<th colspan="3">Scheduled Game Info</th>
+</tr>
+<tr>
+	<td><strong>Game ID:</strong> 5</td>
+	<td><strong>Date:</strong> 5/05/2014</td>
+	<td><strong>Time:</strong> 6:45PM</td>
+</tr>
+<tr>
+	<td><strong>Home Team:</strong> <em>HomeTeam</em></td>
+	<td></td>
+	<td><strong>Away Team:</strong> <em>AwayTeam</em></td>
+</tr>
+</table>
+
+<br />
+
+<table id="game_table">
+<tr>
+	<th colspan="4">Game Data</th>
+</tr>
+<tr>
+	<td><strong>Referee 1</strong></td>
+	<td><strong>Referee 2</strong></td>
+	<td><strong>Statkeeper</strong></td>
+	<td><strong>Start Time</strong></td>
+</tr>
+<tr>
+	<td>
+		<select name="referee1">
+		<option>Select referee</option>
+		<option value="0">- No referee -</option>
+		<option value="1">Justin Sturtz</option>
+		<option value="2">Derek Something</option>
+		</select>
+	</td>
+	<td>
+		<select name="referee2">
+		<option>Select referee</option>
+		<option value="0">- No referee -</option>
+		<option value="1">Justin Sturtz</option>
+		<option value="2">Derek Something</option>
+		</select>
+	</td>
+	<td>
+		<select name="statkeeper">
+		<option>Select statkeeper</option>
+
+		<!-- TODO: Add option for no statkeeper -->
+		<option value="0">- No statkeeper -</option>
+		<?php
+		include ("../../setup/db_setup.php");
+
+		// Pull statkeepers from the database
+		$connection = mysqli_connect($server, $username, $password, $database) or die ("Connection failed");
+
+		$query = "SELECT s.id, concat(p.first, ' ', p.last) as 'name' FROM staff as s JOIN person as p ON s.person = p.id WHERE s.active = 1 ORDER BY p.last, p.first";
+		$result = mysqli_query($connection, $query) or die("Query failed");
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			$id = $row['id'];
+			$name = $row['name'];
+
+			echo "<option value=\"";
+			echo htmlentities($id);
+			echo "\">";
+			echo htmlentities($name);
+			echo "</option>";
+		}
+		mysqli_free_result($result);
+		mysqli_close($connection);
+		?>
+		
+		</select>
+	</td>
+	<td><input type="text" name="startTime"></td>
+</tr>
+<tr>
+	<td><strong><em>Home</em> Score:</strong></td>
+	<td><input type="text" name="homescore" maxlength="2" size="2"></td>
+	<td><strong><em>Away</em> Score:</strong></td>
+	<td><input type="text" name="awayscore" maxlength="2" size="2"></td>
+</tr>
+<tr>
+	<td colspan="2"><input type="checkbox" name="shootout" value="homeSOL"> <em>Home team</em> Lost in Shootout</td>
+	<td colspan="2"><input type="checkbox" name="shootout" value="awaySOL"> <em>Away Team</em> Lost in Shootout</td>
+</tr>
+<tr>
+	<td colspan="2"><input type="checkbox" name="forfeit" value="homeforfeit"> <em>Home Team</em> Forfeited Game</td>
+	<td colspan="2"><input type="checkbox" name="forfeit" value="awayforfeit"> <em>Away Team</em> Forfeited Game</td>
+</tr>
+<tr colspan=4>
+	<td>Game Notes:</td>
+	<td colspan="3"><textarea name="gamenotes" cols="50" rows="5"></textarea></td>
+</tr>
+</table>
+
+<br />
+
+<table id="goalie_table">
+<tr>
+	<th colspan="4">Goalie Data</th>
+</tr>
+<tr>
+	<td colspan="2"><strong><em>Home Team's</em> Goalie</strong></td>
+	<td colspan="2"><strong><em>Away Team's</em> Goalie</strong></td>
+</tr>
+<tr>
+	<td colspan="2">
+		<select name="homegoalie">
+		<option>Select goalie</option>
+		<option>- No goalie -</option>
+		<option>Tony Hansen</option>
+		<option>Scott Anderson</option>
+		<option>Rich Pentico</option>
+		</select>
+	</td>
+	<td colspan="2">
+		<select name="awaygoalie">
+		<option>Select goalie</option>
+		<option>- No goalie -</option>
+		<option>Tony Hansen</option>
+		<option>Scott Anderson</option>
+		<option>Rich Pentico</option>
+		</select>
+	</td>
+</tr>
+<tr>
+	<td><strong>Goals Scored on <em>GoalieName</em>:</strong></td>
+	<td><input type="text" name="homeGA" maxlength="2" size="2"></td>
+	<td><strong>Goals Scored on <em>GoalieName</em>:</strong></td>
+	<td><input type="text" name="awayGA" maxlength="2" size="2"></td>
+</tr>
+<tr>
+	<td colspan="2"><input type="checkbox" name="fullGame" value="homeGoalie" checked="true"> <em>GoalieName</em> played full game?</td>
+	<td colspan="2"><input type="checkbox" name="fullGame" value="homeGoalie" checked="true"> <em>GoalieName</em> played full game?</td>
+</tr>
+</table>
+
+</body>
+</html>
